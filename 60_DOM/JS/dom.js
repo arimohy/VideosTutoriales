120593a32ba6dd01058e260266212d61cf107840
arimohy <arimohy@gmail.com>
2021-01-12 16:22:24 -0500

    video 61 introduccion al DOM

---
+++ 60_DOM/JS/dom.js
@@ -0,0 +1,15 @@
+/*console.log(window)
+        let texto="aprendiendo javascsript hola yhomira";
+        const hablar=(texto)=>speechSynthesis.
+        speak(new SpeechSynthesisUtterance(texto))
+
+        hablar(texto);
+*/
+console.log("******Elementos*******")
+console.log(document)
+console.log(document.head)
+console.log(document.body)
+console.log(document.documentElement)
+console.log(document.forms)
+console.log(document.scripts);
+document.write("<h2>Hola mundo jajajaja </h2>")
