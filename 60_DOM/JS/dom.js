78bd60077ee8df170bc6cba61f96fdf50b9a4f1f
arimohy <arimohy@gmail.com>
2021-01-14 13:10:32 -0500

    video 62 DOM

--- 60_DOM/JS/dom.js
+++ 60_DOM/JS/dom.js
@@ -4,7 +4,7 @@
         speak(new SpeechSynthesisUtterance(texto))
 
         hablar(texto);
-*/
+
 console.log("******Elementos*******")
 console.log(document)
 console.log(document.head)
@@ -12,4 +12,18 @@ console.log(document.body)
 console.log(document.documentElement)
 console.log(document.forms)
 console.log(document.scripts);
-document.write("<h2>Hola mundo jajajaja </h2>")
+document.write("<h2>Hola mundo jajajaja </h2>")*/
+
+console.log(document.getElementsByTagName("li"));
+console.log(document.getElementsByClassName("card"))
+console.log(document.getElementsByName("nombre"))
+console.log(document.getElementById("menu"))
+
+console.log(document.querySelector("#menu"))
+console.log(document.querySelector("a"))
+console.log(document.querySelectorAll("a"))
+document.querySelectorAll("a").forEach((el)=>console.log(el))
+console.log(document.querySelector(".card"))
+console.log(document.querySelectorAll(".card"))
+console.log(document.querySelectorAll(".card")[2])
+console.log(document.querySelector("menu li"))
