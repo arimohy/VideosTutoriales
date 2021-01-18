f76f2d797ded3d6626a48e1cf8406b6926c6d01d
arimohy <arimohy@gmail.com>
2021-01-17 20:35:32 -0500

    video 64 styles  DOM

--- 60_DOM/JS/dom.js
+++ 60_DOM/JS/dom.js
@@ -28,6 +28,8 @@ console.log(document.querySelectorAll(".card"))
 console.log(document.querySelectorAll(".card")[2])
 console.log(document.querySelector("menu li"))*/
 
+//DATA ATRIBUTOS VIDEO 63
+/*
 console.log(document.documentElement.lang)
 
 console.log(document.documentElement.getAttribute("lang"))
@@ -58,6 +60,44 @@ console.log($linkDOM.dataset.description)
 console.log($linkDOM.hasAttribute("data-id"))
 console.log($linkDOM.removeAttribute("data-id"))
 console.log($linkDOM.hasAttribute("data-id"))
+*/
+
+const $linkDOM=document.querySelector(".link-dom");
+
+console.log($linkDOM.style)
+console.log($linkDOM.getAttribute("style"))
+console.log($linkDOM.style.backgroundColor)
+console.log($linkDOM.style.color)
+console.log(window.getComputedStyle($linkDOM))
+console.log(getComputedStyle($linkDOM).getPropertyValue("color"));
+
+$linkDOM.style.setProperty("text-decoration","none")
+$linkDOM.style.setProperty("display","block")
+$linkDOM.style.width="50%";
+$linkDOM.style.textAlign="center";
+$linkDOM.style.marginLeft="auto";
+$linkDOM.style.marginRight="auto";
+$linkDOM.style.padding="1rem";
+$linkDOM.style.borderRadius=".5rem";
+
+console.log($linkDOM)
+//variables css custo, propi
+
+const $html=document.documentElement,
+$body=document.body;
+
+let varDarkColor=getComputedStyle($html).getPropertyValue("--dark-color"),
+varYellowColor=getComputedStyle($html).getPropertyValue("--yellow-color");
+
+console.log(varDarkColor,varYellowColor)
+
+$body.style.backgroundColor=varDarkColor;
+$body.style.color=varYellowColor;
+
+$html.style.setProperty("--dark-color","#000")
+varDarkColor=getComputedStyle($html).getPropertyValue("--dark-color");
+$body.style.setProperty("background-color",varDarkColor)
+
 
 
 
