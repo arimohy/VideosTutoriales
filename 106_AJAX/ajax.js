b2f5f80b3f90c451f2f3da9c13542843e00f87c5
arimohy <arimohy@gmail.com>
2021-02-06 11:00:19 -0500

    video axios 1

--- 106_AJAX/ajax.js
+++ 106_AJAX/ajax.js
@@ -43,7 +43,7 @@
     }) */
     .then((res) => res.ok ? res.json() : Promise.reject(res))
     .then((json) => {
-      console.log(json);
+      //console.log(json);
       //$fetch.innerHTML = json;
       json.forEach(el => {
         const $li = document.createElement("li");
@@ -65,5 +65,59 @@
 
 })();
 (() => {
+  const $fetchasync = document.getElementById("fetch-async"),
+    $fragment = document.createDocumentFragment();
+
+  async function getData() {
+    try {
+      let res = await fetch("https://jsonplaceholder.typicode.com/users"),
+        json = await res.json();
+      if (!res.ok) throw {
+        status: res.status,
+        statusText: res.statusText
+      }
+
+      json.forEach(el => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name}--${el.email}--${el.phone}`;
+        $fragment.appendChild($li);
+      });
+      $fetchasync.appendChild($fragment);
+    } catch (err) {
+      let message = err.statusText || "Ocurrio un error";
+      $fetchasync.innerHTML = `Error....${err.status}: ${message}`;
+    } finally {
+      console.log("este es el finally")
+    }
+
+
+
+  }
+  getData();
+
+})();
+
+(() => {
+  const $axios = document.getElementById("axios"),
+    $fragment = document.createDocumentFragment();
+
+  axios.get("https://jsonplaceholder.typicode.com/user")
+    .then(res => {
+      console.log(res);
+      let json = res.data;
+
+      json.forEach(el => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name}--${el.email}--${el.phone}`;
+        $fragment.appendChild($li);
+      });
+      $axios.appendChild($fragment);
+    }).catch(err => {
+      console.log(err.respose)
+      let message = err.response.statusText || "Ocurrio un error";
+      $axios.innerHTML = `Error....${err.response.status}: ${message}`;
+    }).finally(() => {
+      console.log("Esto se ejecutara independientemete")
+    });
 
 })();
