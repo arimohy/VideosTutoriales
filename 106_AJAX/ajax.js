5445510d8bdbef0a5d3807c2b174e950084001c0
arimohy <arimohy@gmail.com>
2021-02-06 09:49:13 -0500

    video fetch 10

---
+++ 106_AJAX/ajax.js
@@ -0,0 +1,69 @@
+(() => {
+  const xhr = new XMLHttpRequest(),
+    $xhr = document.getElementById("xhr"),
+    $fragment = document.createDocumentFragment();
+
+  xhr.addEventListener("readystatechange", e => {
+    if (xhr.readyState !== 4) { //solo cuando es 4 ya esta en el estado completado
+      return (xhr)
+    }
+    if (xhr.status >= 200 && xhr.status < 300) {
+      //console.log(xhr)
+      //console.log("exito")
+      let json = JSON.parse(xhr.responseText)
+      //console.log(json);
+
+      json.forEach(el => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name}--${el.email}--${el.phone}`;
+        $fragment.appendChild($li);
+      });
+      $xhr.appendChild($fragment)
+    } else {
+      //console.log("error")
+      let message = xhr.statusText || "Ocurrio un error";
+      $xhr.innerHTML = `Error....${xhr.status}: ${message}`
+    }
+    //console.log(xhr)
+    console.log("Este mensaje cargara de todas formas...........")
+  });
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+
+  xhr.send();
+
+})();
+
+(() => {
+  const $fetch = document.getElementById("fetch"),
+    $fragment = document.createDocumentFragment();
+  fetch("https://jsonplaceholder.typicode.com/users")
+    /* .then((res) => {
+      console.log(res);
+      return res.ok ? res.json() : Promise.reject(res)
+    }) */
+    .then((res) => res.ok ? res.json() : Promise.reject(res))
+    .then((json) => {
+      console.log(json);
+      //$fetch.innerHTML = json;
+      json.forEach(el => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name}--${el.email}--${el.phone}`;
+        $fragment.appendChild($li);
+      });
+      $fetch.appendChild($fragment)
+
+    })
+    .catch((err) => {
+      //console.log(err);
+      let message = err.statusText || "Ocurrio un error";
+      $fetch.innerHTML = `Error....${err.status}: ${message}`;
+    }).finally(() => {
+      console.log("esto se ejecutara simepre por la promesa de fetch")
+    });
+
+
+
+})();
+(() => {
+
+})();
